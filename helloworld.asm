94c5ee70f61c536aabff5b3a9b0557ef17a584ea
Gilberto Ayala <gilbertoayala12@gmail.com>
2017-02-25 19:27:06 -0700

    adding hello world :rage:

---
+++ helloworld.asm
@@ -0,0 +1,18 @@
+; holamundo.asm
+; imprime "hola mundo"
+; autor: yo mero
+; fecha: 13 de setpiembre de 2016
+section .data
+	msj DB 'Hola mundo!', 0xA,0x0 ;mensaje a imprimir je
+	lon EQU $ - msj ; longitud del mensaje
+
+section .text
+	GLOBAL _start ; punto de inicio
+_start:
+	mov EDX, lon ; logitud del mensaje tal vex
+	mov ECX, msj ; el mensaje 
+	mov EBX, 1 ;descriptor
+	mov EAX, 4  ;llamada
+	int 0x80 ;llamada 
+	mov EAX, 1 ;llamada
+	int 0x80 ; llamada
