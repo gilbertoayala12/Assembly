6c6fa288287601422786cb5e96a24f86504a0dd2
Gilberto Ayala <gilbertoayala12@gmail.com>
2017-04-06 11:52:48 -0700

    arreglo entero semicompleto falta promedio

--- arreglo_entero.asm
+++ arreglo_entero.asm
@@ -1,4 +1,4 @@
-; arreglo enteros
+; arreglo enteros y sacar el promedio
 ; gilberto ayala
 ; 6 de abril de 2017
 %include 'funciones2.asm'
@@ -8,8 +8,36 @@ segment .bss
 section .text
 	global _start
 _start:
+	pop ecx 	; obtenemos el numero de argumentos
+	cmp ecx, 2	; comparamos si es menor a 2
+	jl salida	; salimos si es menor a 2
+
+	pop eax 	; obtenemos el nombre del programa
+	dec ecx		; restamos 1 al numero de argumentos
+	mov edx, ecx
+	; mov edx, 0 	; ponemos en 0 edx
+	mov esi, array 	; la direccion de array a esi
 
 ciclo:
-; copiar programa despues del pop eax call atoi, add esi, 4 
+	; copiar programa despues del pop eax call atoi, add esi, 4 
+	pop eax
+	call atoi 
+	mov [esi], eax
+	add esi, 4
+	dec ecx 
+	cmp ecx,0
+	jne ciclo 
+	mov ecx, edx
+	mov esi, array
+
 impresion:
-; add esi 4 
+	mov eax, [esi] 	; apuitnamos a donde esta
+	call iprintLF	; print
+	add esi, 4
+
+	dec ecx
+	cmp ecx, 0
+	jne impresion
+	; add esi 4 
+salida:
+	jmp quit 		; salida
